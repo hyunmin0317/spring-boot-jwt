f859bc6ca83d8dd8865c6e5bc6501093a6eb9256
hyunmin0317 <choihm9903@naver.com>
2024-08-03 19:59:33 +0900

    feat: EnableJpaAuditing 어노테이션 추가 (#5)

--- src/main/java/com/hyunmin/jwt/SpringBootJwtApplication.java
+++ src/main/java/com/hyunmin/jwt/SpringBootJwtApplication.java
@@ -2,12 +2,13 @@ package com.hyunmin.jwt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class SpringBootJwtApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootJwtApplication.class, args);
     }
-
 }
