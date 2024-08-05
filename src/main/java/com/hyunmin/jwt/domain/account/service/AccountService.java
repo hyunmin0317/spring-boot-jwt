5b9d27ceff47f59b65368cf98701cfd49b157cf1
hyunmin0317 <choihm9903@naver.com>
2024-08-05 21:26:27 +0900

    refactor: 중복된 username 검증 로직 추가 (#13)

--- src/main/java/com/hyunmin/jwt/domain/account/service/AccountService.java
+++ src/main/java/com/hyunmin/jwt/domain/account/service/AccountService.java
@@ -4,22 +4,32 @@ import com.hyunmin.jwt.domain.account.dto.RegisterRequestDto;
 import com.hyunmin.jwt.domain.account.dto.RegisterResponseDto;
 import com.hyunmin.jwt.domain.account.entity.Member;
 import com.hyunmin.jwt.domain.account.repository.MemberRepository;
-import jakarta.transaction.Transactional;
+import com.hyunmin.jwt.global.exception.RestException;
+import com.hyunmin.jwt.global.exception.code.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class AccountService {
 
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
 
+    @Transactional
     public RegisterResponseDto register(RegisterRequestDto requestDto) {
+        validateUsername(requestDto.username());
         String encodedPw = passwordEncoder.encode(requestDto.password());
         Member member = requestDto.toEntity(encodedPw);
         return RegisterResponseDto.from(memberRepository.save(member));
+    }
+
+    private void validateUsername(String username) {
+        if (memberRepository.existsByUsername(username)) {
+            throw new RestException(ErrorCode.ACCOUNT_CONFLICT);
+        }
     }
 }
