c325f9d28ed3cfacccdd7e282130a9e49124b677
hyunmin0317 <choihm9903@naver.com>
2024-08-06 22:43:17 +0900

    feat: JWT 인증 필터 등록 (#17)

--- src/main/java/com/hyunmin/jwt/global/security/config/SecurityConfig.java
+++ src/main/java/com/hyunmin/jwt/global/security/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.hyunmin.jwt.global.security.config;
 
+import com.hyunmin.jwt.global.security.filter.JwtAuthenticationFilter;
+import com.hyunmin.jwt.global.security.provider.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,10 +13,14 @@ import org.springframework.security.config.annotation.web.configurers.HeadersCon
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -35,6 +42,9 @@ public class SecurityConfig {
                 .requestMatchers("/api/v1/accounts/**").permitAll()
                 .anyRequest().authenticated()
         );
+
+        // JWT 기반 인증을 처리하기 위해 JwtAuthenticationFilter를 UsernamePasswordAuthenticationFilter 이전에 추가
+        http.addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
