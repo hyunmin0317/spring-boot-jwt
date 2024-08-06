679cc7786c6c967ed82b7b26de340cef4764bc7f
hyunmin0317 <choihm9903@naver.com>
2024-08-06 22:42:42 +0900

    feat: JWT 인증 메서드 개발 (#17)

--- src/main/java/com/hyunmin/jwt/global/security/provider/JwtTokenProvider.java
+++ src/main/java/com/hyunmin/jwt/global/security/provider/JwtTokenProvider.java
@@ -1,14 +1,28 @@
 package com.hyunmin.jwt.global.security.provider;
 
 import com.hyunmin.jwt.global.common.entity.enums.MemberRole;
+import com.hyunmin.jwt.global.exception.RestException;
+import com.hyunmin.jwt.global.exception.code.ErrorCode;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
+import java.util.Collection;
 import java.util.Date;
+import java.util.List;
 
 @Component
 public class JwtTokenProvider {
@@ -32,6 +46,31 @@ public class JwtTokenProvider {
                 .compact();
     }
 
+    public String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        return StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ") ?
+                bearerToken.substring(7) : null;
+    }
+
+    public boolean validateToken(String token, boolean isRefresh) {
+        try {
+            getClaims(token);
+            return true;
+        } catch (ExpiredJwtException e) {
+            throw new RestException(isRefresh ? ErrorCode.RELOGIN_EXCEPTION : ErrorCode.EXPIRED_JWT_EXCEPTION);
+        } catch (JwtException | IllegalArgumentException e) {
+            throw new RestException(ErrorCode.INVALID_TOKEN_EXCEPTION);
+        }
+    }
+
+    public Authentication getAuthentication(String token) {
+        Claims claims = getClaims(token);
+        String memberRole = claims.get("memberRole").toString();
+        Collection<? extends GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority(memberRole));
+        User principal = new User(claims.getSubject(), "", authorities);
+        return new UsernamePasswordAuthenticationToken(principal, token, authorities);
+    }
+
     private SecretKey generateSecretKeySpec(String secret) {
         byte[] keyBytes = secret.getBytes(StandardCharsets.UTF_8);
         return new SecretKeySpec(keyBytes, Jwts.SIG.HS256.key().build().getAlgorithm());
@@ -40,5 +79,9 @@ public class JwtTokenProvider {
     private Date expirationDate() {
         Date now = new Date();
         return new Date(now.getTime() + this.accessExpirationTime);
+    }
+
+    private Claims getClaims(String token) {
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload();
     }
 }
