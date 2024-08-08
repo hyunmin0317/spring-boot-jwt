b60b35642c72e158ea408fad4d9538747d73f986
hyunmin0317 <choihm9903@naver.com>
2024-08-08 19:57:41 +0900

    feat: 전체 사용자 정보 조회 API (#18)

--- src/main/java/com/hyunmin/jwt/domain/member/controller/MemberController.java
+++ src/main/java/com/hyunmin/jwt/domain/member/controller/MemberController.java
@@ -2,14 +2,27 @@ package com.hyunmin.jwt.domain.member.controller;
 
 import com.hyunmin.jwt.domain.member.dto.MemberInfoResponseDto;
 import com.hyunmin.jwt.global.common.entity.Member;
+import com.hyunmin.jwt.global.common.service.MemberQueryService;
 import com.hyunmin.jwt.global.security.annotation.AuthMember;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/v1/members")
 public class MemberController {
+
+    private final MemberQueryService memberQueryService;
+
+    @GetMapping
+    public Page<MemberInfoResponseDto> findAll(@PageableDefault(size = 5) Pageable pageable) {
+        return memberQueryService.findAll(pageable);
+    }
 
     @GetMapping("/info")
     public MemberInfoResponseDto memberInfo(@AuthMember Member member) {
--- src/main/java/com/hyunmin/jwt/global/common/service/MemberQueryService.java
+++ src/main/java/com/hyunmin/jwt/global/common/service/MemberQueryService.java
@@ -1,10 +1,13 @@
 package com.hyunmin.jwt.global.common.service;
 
+import com.hyunmin.jwt.domain.member.dto.MemberInfoResponseDto;
 import com.hyunmin.jwt.global.common.entity.Member;
 import com.hyunmin.jwt.global.common.repository.MemberRepository;
 import com.hyunmin.jwt.global.exception.GeneralException;
 import com.hyunmin.jwt.global.exception.code.ErrorCode;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,5 +21,10 @@ public class MemberQueryService {
     public Member findByUsername(String username) {
         return memberRepository.findByUsername(username)
                 .orElseThrow(() -> new GeneralException(ErrorCode.ACCOUNT_NOT_FOUND));
+    }
+
+    public Page<MemberInfoResponseDto> findAll(Pageable pageable) {
+        Page<Member> memberPage = memberRepository.findAll(pageable);
+        return MemberInfoResponseDto.from(memberPage);
     }
 }
