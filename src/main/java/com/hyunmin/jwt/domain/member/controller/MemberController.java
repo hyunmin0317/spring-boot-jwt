080504ae72e52710c7d714277d454edd1adc8337
hyunmin0317 <choihm9903@naver.com>
2024-08-08 19:18:58 +0900

    refactor: 코드 리팩토링 (#18)

--- src/main/java/com/hyunmin/jwt/domain/member/controller/MemberController.java
+++ src/main/java/com/hyunmin/jwt/domain/member/controller/MemberController.java
@@ -3,7 +3,6 @@ package com.hyunmin.jwt.domain.member.controller;
 import com.hyunmin.jwt.domain.member.dto.MemberInfoResponseDto;
 import com.hyunmin.jwt.global.common.entity.Member;
 import com.hyunmin.jwt.global.security.annotation.AuthMember;
-import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController {
 
     @GetMapping("/info")
-    @Parameter(name = "member", hidden = true)
     public MemberInfoResponseDto memberInfo(@AuthMember Member member) {
         return MemberInfoResponseDto.from(member);
     }
