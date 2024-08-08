c6ca4303cd550d1dfddc1dac5bac954b0da92b8b
Hyunmin Choi <choihm9903@naver.com>
2024-08-08 19:24:49 +0900

    [FEATURE] 로그인한 사용자 정보 조회 API (#27)
    * feat: AuthMember 어노테이션 (#18)
    
    * feat: AuthMember 어노테이션 resolver 클래스 (#18)
    
    * feat: Member 조회 서비스 (#18)
    
    * feat: 로그인한 사용자 정보 조회 API (#18)
    
    * refactor: 코드 리팩토링 (#18)
    
    * feat: 커스텀 Argument Resolver 추가 (#18)

--- src/main/java/com/hyunmin/jwt/domain/member/controller/MemberController.java
+++ src/main/java/com/hyunmin/jwt/domain/member/controller/MemberController.java
@@ -1,5 +1,8 @@
 package com.hyunmin.jwt.domain.member.controller;
 
+import com.hyunmin.jwt.domain.member.dto.MemberInfoResponseDto;
+import com.hyunmin.jwt.global.common.entity.Member;
+import com.hyunmin.jwt.global.security.annotation.AuthMember;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/members")
 public class MemberController {
 
-    @GetMapping
-    public String test() {
-        return "test";
+    @GetMapping("/info")
+    public MemberInfoResponseDto memberInfo(@AuthMember Member member) {
+        return MemberInfoResponseDto.from(member);
     }
 }
