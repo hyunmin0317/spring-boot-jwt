1e752e6a71d08497b4b61796bceb0e126bc183aa
hyunmin0317 <choihm9903@naver.com>
2024-08-08 20:16:42 +0900

    feat: 스웨거 파라미터 설정 (#19)

--- src/main/java/com/hyunmin/jwt/domain/member/controller/MemberController.java
+++ src/main/java/com/hyunmin/jwt/domain/member/controller/MemberController.java
@@ -5,9 +5,9 @@ import com.hyunmin.jwt.global.common.entity.Member;
 import com.hyunmin.jwt.global.common.service.MemberQueryService;
 import com.hyunmin.jwt.global.security.annotation.AuthMember;
 import lombok.RequiredArgsConstructor;
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +20,7 @@ public class MemberController {
     private final MemberQueryService memberQueryService;
 
     @GetMapping
-    public Page<MemberInfoResponseDto> findAll(@PageableDefault(size = 5) Pageable pageable) {
+    public Page<MemberInfoResponseDto> findAll(@ParameterObject Pageable pageable) {
         return memberQueryService.findAll(pageable);
     }
 
