340398d7b1610699044a84f67284669bca5ae1e6
hyunmin0317 <choihm9903@naver.com>
2024-09-01 09:42:50 +0900

    feat: 비밀번호 변경 메서드 (#35)

--- src/main/java/com/hyunmin/jwt/domain/member/service/MemberCommandService.java
+++ src/main/java/com/hyunmin/jwt/domain/member/service/MemberCommandService.java
@@ -1,7 +1,13 @@
 package com.hyunmin.jwt.domain.member.service;
 
+import com.hyunmin.jwt.domain.member.dto.ChangePasswordRequestDto;
+import com.hyunmin.jwt.domain.member.dto.MemberInfoResponseDto;
+import com.hyunmin.jwt.global.common.entity.Member;
 import com.hyunmin.jwt.global.common.repository.MemberRepository;
+import com.hyunmin.jwt.global.exception.GeneralException;
+import com.hyunmin.jwt.global.exception.code.ErrorCode;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,8 +17,16 @@ import org.springframework.transaction.annotation.Transactional;
 public class MemberCommandService {
 
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public void deleteMember(Long id) {
         memberRepository.deleteById(id);
+    }
+
+    public MemberInfoResponseDto changePassword(Long id, ChangePasswordRequestDto requestDto) {
+        Member member = memberRepository.findById(id).orElseThrow(() -> new GeneralException(ErrorCode.MEMBER_NOT_FOUND));
+        String newEncodedPw = passwordEncoder.encode(requestDto.password());
+        member.changePassword(newEncodedPw);
+        return MemberInfoResponseDto.from(member);
     }
 }
